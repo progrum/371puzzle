b94d76b737e6a860913243fe9da0e527db3a661c
progrum <robert.banks@rutgers.edu>
2013-11-13 19:41:33 -0500

    removed block class, stored in separate file

--- project.py
+++ project.py
@@ -97,21 +97,7 @@ class Puzzle:
 #        string = self.size
 #        return 'Tray is size'+' '+str(string)
 
-class Block:
-	def __init__(self, size, position):
-		self.size = size
-		self.position = position
-		
-	def block_position(self):
-		return self.position
-		
-	def block_size(self):
-		return self.size
-		
-	def __str__(self):
-		size_string = str(self.size)
-		position_string = str(self.position)
-		return 'Size:'+size_string+', Position:'+position_string
+
 
 
 
