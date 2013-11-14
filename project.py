024b400c0d50b8a1dc7c45348adee602853c1429
progrum <robert.banks@rutgers.edu>
2013-11-13 21:40:41 -0500

    updates again

--- project.py
+++ project.py
@@ -101,6 +101,16 @@ gofile = str(sys.argv[3])
 A = op_file(confile)
 B = op_file(gofile)
 
+print A
+print A[0][0]
+print A[0][2]
+# Tray((A[0][0],A[0][2]), ListOfBlocks, numofblocks)
+# Block class gets A[1:]; list of strings of input from file
+# Tray class gets A[0]; tray size (row column)
+
+
+
+
 block_dictionary = create_dictionary(A)
 block_list = create_list(block_dictionary)
 print block_dictionary
