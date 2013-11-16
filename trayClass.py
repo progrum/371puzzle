8333d6e4e92438ce09388ee9f12937d76676a10c
progrum <robert.banks@rutgers.edu>
2013-11-16 12:18:10 -0500

    updates to tray class

--- trayClass.py
+++ trayClass.py
@@ -2,46 +2,50 @@
 from block_class import *                 
 
 class Tray:
-    def __init__(self, size, numOfBlks, listOfBlks ):
-        #attributes of a Tray Object
-                self.length = size[1] #getLength(), columns
-                self.width = size[0]   #getWidth(), rows 
-                self.numOfBlks = numOfBlks
-                self.listOfBlks = listOfBlks  #new: getList of block objects
-                self.matrix = self.createMatrix(size[1],size[0])
-                
-                
-    def length(self):
-                return self.length
-    def width(self):
-                return self.width
-                
-    def numOfBlks(self):
-                return self.numOfBlks
-            
-    def __str__(self):
-        size_string = str(self.length) +","+ str(self.width)
-        numBlk_string = str(self.numOfBlks)   
-        return 'Size:'+size_string+', NumberOfBlocks '+numBlk_string + str(self.matrix)
+	def __init__(self, size, numOfBlks, listOfBlks ):
+	#attributes of a Tray Object
+		self.width = size[1] #getLength(), columns
+		self.length = size[0]   #getWidth(), rows 
+		self.numOfBlks = numOfBlks
+		self.listOfBlks = listOfBlks  #new: getList of block objects
+		self.matrix = self.createMatrix(size[1],size[0])
+		
+	def length(self):
+		return self.length
+		
+	def width(self):
+		return self.width
+		
+	def numOfBlks(self):
+		return self.numOfBlks
+		
+	def __str__(self):
+		size_string = str(self.length) +","+ str(self.width)
+		numBlk_string = str(self.numOfBlks)   
+		return 'Size:'+size_string+', NumberOfBlocks '+numBlk_string + str(self.matrix)
         
-    def createMatrix(self,width,length):
-        print 'In create Matrix'
+	def createMatrix(self,length,width):
+		print 'In create Matrix'
 		# Update: initiated matrix to zero, and rearranged the loop.
-        M = [[0 for i in range(length)] for j in range(width)]
-        print ('End of create Matrix')
-        return  M
-         
-    def isFreeSpace(self,blk):
+		M = [[0 for i in range(width)] for j in range(length)]
+		print ('End of create Matrix')
+		return  M
+			
+	# Function to determine.
+	def isFreeSpace(self,blk):
         
-        directions = {"right" : 0 , "left" : 0, "up" : 0, "down" : 0}
-        
+		directions = {"right" : 0 , "left" : 0, "up" : 0, "down" : 0}
         # returns direction dictionary that we can move
-        return directions
+		return directions
 		
+	# Function to map the blocks to a dictionary	
+	def map_blocks(self):
 		
-	def map_blocks(self, listOfBlks):
-		for blk in listOfBlks:
-			tray_dict[blk.position] = blk
+		tray_dict = {}
+		print 'in map blocks'
+		for blk in self.listOfBlks:
+			tray_dict[(blk.position[0],blk.position[1])] = blk
+		print 'out map blocks'
 		return tray_dict
     
 	
@@ -52,11 +56,26 @@ A[0].set_position(1,2)
 print A[0]
 B = Tray([5,10], len(A), A)
 print B
+
 C = B.isFreeSpace(A[0])
 print C
 # Below to print a formatted 3x4 matrix
 D = B.createMatrix(3,4)
 for x in D:
 	print x
-E = B.map_blocks
-print E
+
+print B.listOfBlks[0].position
+F = B.map_blocks()
+print F
+"""
+tray_dict = {}
+for blk in B.listOfBlks:
+	tray_dict[(blk.position[0],blk.position[1])] = blk
+print tray_dict
+"""
+"""
+A = Tray([5,5], 1, [(4,5),[1,1]])
+print A
+print A.isFreeSpace(((4,3),[0,0]))
+A.map_blocks()
+"""
